d72eaf0e945ff0ac9088e725c511a6adeeb8b20d
Ermolin Dmitry <145351448+Kotto-von-Bismarck@users.noreply.github.com>
2024-11-04 10:04:06 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
             e.target.parentElement.parentElement.firstElementChild.classList.toggle('catalogCard__item_active');
             if (e.target.parentElement.parentElement.firstElementChild.classList.contains('catalogCard__item_active')) {
                 e.target.innerHTML = '<span>Назад</span>'
-                e.target.parentElement.parentElement.style.cssText = 'background: url(../images/G3.png) center center/cover no-repeat'
+                e.target.parentElement.parentElement.style.cssText = 'background: url(images/G3.png) center center/cover no-repeat'
             } else {
                 e.target.innerHTML = '<span>Узнать больше</span>' 
                 e.target.parentElement.parentElement.style.cssText = 'background: rgba(0, 0, 0, 0.5)'
